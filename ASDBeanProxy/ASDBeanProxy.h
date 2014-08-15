a1bf3fd5d2c74e40ae6a01c96447205bfb780e10
Mike Hagedorn <mike@silverchairsolutions.com>
2014-08-14 22:51:22 -0400

    Initial Commit

---
+++ ASDBeanProxy/ASDBeanProxy.h
@@ -0,0 +1,13 @@
+//
+//  ASDBeanProxy.h
+//  ASDBeanProxy
+//
+//  Created by Mike Hagedorn on 8/14/14.
+//  Copyright (c) 2014 appsdynamic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASDBeanProxy : NSObject
+
+@end
---
+++ ASDBeanProxy/ASDBeanProxy.m
@@ -0,0 +1,13 @@
+//
+//  ASDBeanProxy.m
+//  ASDBeanProxy
+//
+//  Created by Mike Hagedorn on 8/14/14.
+//  Copyright (c) 2014 appsdynamic. All rights reserved.
+//
+
+#import "ASDBeanProxy.h"
+
+@implementation ASDBeanProxy
+
+@end
